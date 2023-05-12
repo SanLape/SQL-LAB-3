3071f9618c34d3108182120aff4f08add10820e4
Santiago Lapeña <slapena88@gmail.com>
2023-05-12 22:42:05 +0200

    Fin actividad 2.1

--- TP Actividad - 2/Actividad 2.1/Actividad 2.1.sql
+++ TP Actividad - 2/Actividad 2.1/Actividad 2.1.sql
@@ -107,21 +107,140 @@ WHERE
 -- número de teléfono fijo y si no tiene este último el mail. En caso de no tener ninguno
 -- de los tres debe figurar 'Incontactable'.
 
-
+SELECT 
+    Apellidos,
+    Nombres,
+    CASE
+        WHEN Celular IS NOT NULL THEN Celular
+        WHEN Telefono IS NOT NULL THEN Telefono
+        WHEN Mail IS NOT NULL THEN Mail
+        ELSE 'INCONTACTABLE'
+    END AS 'CONTACTO'
+FROM Clientes
 
 -- 12 Apellidos, nombres y medio de contacto de todos los clientes. Si tiene celular debe
 -- figurar 'Celular'. Si no tiene celular pero tiene teléfono fijo debe figurar 'Teléfono fijo'
 -- de lo contrario y si tiene Mail debe figurar 'Email'. Si no posee ninguno de los tres
 -- debe figurar NULL.
+
+SELECT
+    Apellidos,
+    Nombres,
+    CASE 
+        WHEN Celular IS NOT NULL THEN 'CELULAR'
+        WHEN Telefono IS NOT NULL THEN 'TELEFONO FIJO'
+        WHEN Mail IS NOT NULL THEN 'EMAIL'
+    END AS 'CONTACTO'
+FROM Clientes
+
 -- 13 Todos los datos de los colaboradores que hayan nacido luego del año 2000
+
+SELECT 
+    Legajo,
+    Apellidos,
+    Nombres,
+    ModalidadTrabajo,
+    Sueldo,
+    FechaNacimiento,
+    AñoIngreso
+FROM Colaboradores
+WHERE 
+    YEAR(FechaNacimiento) < 2000
+
 -- 14 Todos los datos de los colaboradores que hayan nacido entre los meses de Enero y
 -- Julio (inclusive)
+
+SELECT
+    Legajo,
+    Apellidos,
+    Nombres,
+    ModalidadTrabajo,
+    Sueldo,
+    FechaNacimiento,
+    AñoIngreso
+FROM Colaboradores
+WHERE
+    MONTH(FechaNacimiento) BETWEEN 1 AND 7
+
 -- 15 Todos los datos de los clientes cuyo apellido finalice con vocal
+
+SELECT
+    ID,
+    Apellidos,
+    Nombres,
+    Mail,
+    Telefono,
+    Celular,
+    RegistroWeb,
+    Estado
+FROM Clientes
+WHERE
+    Apellidos LIKE '%[A,E,I,O,U]'
+
 -- 16 Todos los datos de los clientes cuyo nombre comience con 'A' y contenga al menos
 -- otra 'A'. Por ejemplo, Ana, Anatasia, Aaron, etc
+
+SELECT
+    ID,
+    Apellidos,
+    Nombres,
+    Mail,
+    Telefono,
+    Celular,
+    RegistroWeb,
+    Estado
+FROM Clientes
+WHERE
+    Nombres LIKE 'A%A%'
+
 -- 17 Todos los colaboradores que tengan más de 10 años de antigüedad
+
+SELECT
+    Legajo,
+    Apellidos,
+    Nombres,
+    ModalidadTrabajo,
+    Sueldo,
+    FechaNacimiento,
+    AñoIngreso
+FROM Colaboradores
+WHERE
+    DATEDIFF(YEAR,AñoIngreso,GETDATE()) < 10
+
 -- 18 Los códigos de producto, sin repetir, que hayan registrado al menos un pedido
+
+SELECT DISTINCT
+    IDProducto
+FROM Pedidos
+
 -- 19 Todos los datos de todos los productos con su precio aumentado en un 20%
+
+SELECT
+    ID,
+    IDCategoria,
+    Descripcion,
+    DiasConstruccion,
+    Costo,
+    PrecioVentaMayorista,
+    CantidadMayorista,
+    Estado,
+    Precio,
+    Precio * 1.2 AS 'PRECIO + 20% '
+FROM Productos
+
 -- 20 Todos los datos de todos los colaboradores ordenados por apellido
 -- ascendentemente en primera instancia y por nombre descendentemente en segunda
 -- instancia.
+
+SELECT
+    Legajo,
+    Apellidos,
+    Nombres,
+    ModalidadTrabajo,
+    Sueldo,
+    FechaNacimiento,
+    AñoIngreso
+FROM Colaboradores
+ORDER BY 
+    Apellidos ASC,
+    Nombres DESC
